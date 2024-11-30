ec63a908befd1faaef4ed6c4180da388e2ad0757
YDMA970305 <ymartineza@unicartagena.edu.co>
2024-11-30 15:56:26 -0500

    Se crea la  Clase usuario dentro de la carpeta modelo

---
+++ src/main/java/com/Desarrolloweb/Crud/modelo/Usuario.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Desarrolloweb.Crud.modelo;
+import java.io.Serializable;
+
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ *
+ * @author Yesid Martinez
+ */
+
+ @Entity
+ @Table(name = "usuarios",catalog = "ejemplocrudjsp")
+ @Data
+public class Usuario implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String password;
+    private String nombre;
+    private String apellidos;
+    private String rol;
+    
+    private String email;
+    private String telefono;
+    private String estado;
+    @CreationTimestamp
+    private String fecha_registro;
+}
